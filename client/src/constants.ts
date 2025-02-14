4d7cf5ce0e12a4740a161fef6be7b0a2b98ba429
omar <omaraziz.dev@proton.me>
2025-02-14 11:36:41 -0800

    fix: update url

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,5 +1,5 @@
 export const SERVER_URL =
   import.meta.env.VITE_SERVER_URL ??
   (import.meta.env.PROD
-    ? 'https://offline-server-example.evm.workers.dev'
+    ? 'https://exp-0003-server.evm.workers.dev'
     : 'http://localhost:6900')
