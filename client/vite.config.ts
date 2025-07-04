10b17bcad76d2ef33ecff1505ec385a7ab172ca3
o-az <omaraziz.dev@proton.me>
2025-07-04 15:43:54 -0700

    chore: remove dev vite setup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,7 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 
-export default defineConfig((config) => {
-  const env = loadEnv(config.mode, process.cwd(), '')
-  console.info(env.ALLOWED_HOSTS.split(','))
+export default defineConfig((_) => {
   return {
     plugins: [react()],
   }
